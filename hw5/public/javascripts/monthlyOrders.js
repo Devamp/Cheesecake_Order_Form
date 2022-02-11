a407d31354fc7b743408f800f14dfce8f1a81f03
Devamp <Devam29@gmail.com>
2022-02-11 14:29:55 -0800

    finish hw5 part 1

--- hw5/public/javascripts/monthlyOrders.js
+++ hw5/public/javascripts/monthlyOrders.js
@@ -11,12 +11,31 @@ $(function() {
 // function to replace the selected month with the current month being displayed when a new month is clicked
  eventHandler = function(){
    $("#dropButtonID").html(this.innerHTML); // set new selected month
-   $("#myList").empty(); // clear current list
-   $.post('/orders', // url request
+   $("#cherryDisplay").empty(); // clear current list
+   $("#plainDisplay").empty(); // clear current list
+   $("#chocolateDisplay").empty(); // clear current list
+
+   $.post('/orders', convert(this.innerHTML), // url request and the month selected - sent to server
       function(data, status) { // function to execute on success
-         $("#myList").append(data); // append the JSON data that is recieved
+         //var toAppend = data[0]["Topping"];
+         var dataParsed = JSON.parse(data);
+
+         var cherry = dataParsed[2].Topping + " Cheesecakes: " + dataParsed[2].Quantity;
+         var chocolate = dataParsed[1].Topping + " Cheesecakes: " + dataParsed[1].Quantity;
+         var plain = dataParsed[0].Topping + " Cheesecakes: " + dataParsed[0].Quantity;
+
+         $("#cherryDisplay").html(cherry);
+         $("#chocolateDisplay").html(chocolate);
+         $("#plainDisplay").html(plain);
+
       });
  }
+
+ // helper function to format selected month into a JSON format
+function convert(month){
+   var string = {Month : month};
+   return (string);
+}
  
  
  
--- hw5/routes/orders.js
+++ hw5/routes/orders.js
@@ -1,29 +1,192 @@
+
 /* Server side file to maintain the orders
 * Author: Devam Patel
-* Version: 2/2/22
+* Version: 2/11/22
 */
 
 var express = require('express');
+const res = require('express/lib/response');
+const { dbquery } = require('./dbms');
 var router = express.Router();
 
-// create the JSON object array
-let objArray = [
-    {
-        "Topping": "Plain",
-        "Quantity": 2
-    },
 
-    {
-        "Topping": "Chocolate",
-        "Quantity": 4
-    },
-
-    {
-        "Topping": "Cherry",
-        "Quantity": 8
+// helper class to store a month's topping and quantities
+let MonthData = class {
+    constructor(countPlain, countChocolate, countCherry) {
+        this.plain = countPlain;
+        this.chocolate = countChocolate;
+        this.cherry = countCherry;
     }
-]
 
+    addPlain(value){
+        this.plain += value;
+    }
+
+    get getPlain(){
+        return this.plain;
+    }
+
+    addChocolate(value){
+        this.chocolate += value;
+    }
+
+    get getChocolate(){
+        return this.chocolate;
+    }
+
+    addCherry(value){
+        this.cherry += value;
+    }
+
+    get getCherry(){
+        return this.cherry;
+    }
+}
+
+  // initialize all of the month's data classes
+  const janData = new MonthData(0, 0, 0);
+  const febData = new MonthData(0, 0, 0);
+  const marData = new MonthData(0, 0, 0);
+  const aprData = new MonthData(0, 0, 0);
+  const mayData = new MonthData(0, 0, 0);
+  const junData = new MonthData(0, 0, 0);
+  const julData = new MonthData(0, 0, 0);
+  const augData = new MonthData(0, 0, 0);
+  const sepData = new MonthData(0, 0, 0);
+  const octData = new MonthData(0, 0, 0);
+  const novData = new MonthData(0, 0, 0);
+  const decData = new MonthData(0, 0, 0);
+
+// get query
+dbquery("select Topping, Month, count(*) from ORDERS GROUP BY Month, Topping", storeResults);
+
+function storeResults(bool, results){
+
+    // loop through the results and add the given quantities into the respective month's object
+    for(i = 0; i < results.length; i++){
+
+        if(results[i].Month == "JAN"){
+            if(results[i].Topping == 'Plain'){
+                janData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                janData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                janData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "FEB"){
+            if(results[i].Topping == 'Plain'){
+                febData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                febData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                febData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "MAR"){
+            if(results[i].Topping == 'Plain'){
+                marData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                marData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                marData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "APR"){
+            if(results[i].Topping == 'Plain'){
+                aprData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                aprData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                aprData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "MAY"){
+            if(results[i].Topping == 'Plain'){
+                mayData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                mayData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                mayData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "JUN"){
+            if(results[i].Topping == 'Plain'){
+                junData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                junData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                junData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "JUL"){
+            if(results[i].Topping == 'Plain'){
+                julData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                julData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                julData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "AUG"){
+            if(results[i].Topping == 'Plain'){
+                augData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                augData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                augData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "SEP"){
+            if(results[i].Topping == 'Plain'){
+                sepData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                sepData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                sepData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "OCT"){
+            if(results[i].Topping == 'Plain'){
+                octData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                octData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                octData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "NOV"){
+            if(results[i].Topping == 'Plain'){
+                novData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                novData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                novData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+        if(results[i].Month == "DEC"){
+            if(results[i].Topping == 'Plain'){
+                decData.addPlain(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Chocolate'){
+                decData.addChocolate(results[i]['count(*)']);
+            } else if (results[i].Topping == 'Cherry'){
+                decData.addCherry(results[i]['count(*)']);
+            }
+        }
+
+    }
+}
 
 /* GET data for orders page. */
 router.get('/', function(req, res, next) {
@@ -32,7 +195,221 @@ router.get('/', function(req, res, next) {
 
 /* POST new data to orders page*/
 router.post('/', function(req, res, next) {
-    res.send(JSON.stringify(objArray));
+    var toReturn = dispay(req.body.Month); // call helper function to get JSON object
+    res.json(toReturn);
 });
+
+// function which returns the corrsponding JSON object to a given month
+function dispay(monthSelected){
+    
+    if(monthSelected == "Jan"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": janData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": janData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : janData.getCherry
+            }
+        ]);
+     
+    }
+
+    if(monthSelected == "Feb"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": febData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": febData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : febData.getCherry
+            }
+        ]);
+     
+    }
+
+    if(monthSelected == "Mar"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": marData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": marData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : marData.getCherry
+            }
+        ]);
+     
+    }
+
+    if(monthSelected == "Apr"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": aprData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": aprData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : aprData.getCherry
+            }
+        ]);
+    }
+
+    if(monthSelected == "May"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": mayData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": mayData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : mayData.getCherry
+            }
+        ]);
+    }
+
+    if(monthSelected == "Jun"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": junData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": junData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : junData.getCherry
+            }
+        ]);
+    }
+
+    if(monthSelected == "Jul"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": julData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": julData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : julData.getCherry
+            }
+        ]);
+    }
+
+    if(monthSelected == "Aug"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": augData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": augData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : augData.getCherry
+            }
+        ]);
+    }
+
+    if(monthSelected == "Sep"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": sepData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": sepData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : sepData.getCherry
+            }
+        ]);
+    }
+
+    if(monthSelected == "Oct"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": octData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": octData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : octData.getCherry
+            }
+        ]);
+    }
+
+    if(monthSelected == "Nov"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": novData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": novData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : novData.getCherry
+            }
+        ]);
+    }
+
+    if(monthSelected == "Dec"){
+        return JSON.stringify([
+            {
+                "Topping" : "Plain",
+                "Quantity": decData.getPlain
+            },
+            {
+                "Topping" : "Chocolate",
+                "Quantity": decData.getChocolate
+            },
+            {
+                "Topping" : "Cherry",
+                "Quantity" : decData.getCherry
+            }
+        ]);
+    }
+
+}
   
-  module.exports = router;
+
+module.exports = router;
